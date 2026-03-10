d3188866d56ef4f7c21f287947fa39a947d77cc1
NifemiSoneye <nifemisoneye@gmail.com>
2026-03-10 05:35:14 -0700

    React router initalized

--- React-Register/src/App.jsx
+++ React-Register/src/App.jsx
@@ -2,14 +2,30 @@ import { useState } from "react";
 import "./index.css";
 import Register from "./Register";
 import Login from "./Login";
+import { Routes, Route } from "react-router-dom";
 
 function App() {
   const [count, setCount] = useState(0);
 
   return (
-    <main className="App">
-      <Login />
-    </main>
+    <Routes>
+      <Route path="/" element={<Layout />}>
+        {/* Public routes */}
+        <Route path="login" element={<Login />} />
+        <Route path="register" element={<Register />} />
+        <Route path="linkpage" element={<Linkpage />} />
+        <Route path="unauthorized" element={<Unauthorized />} />
+
+        {/* We want to protect these routes */}
+        <Route path="/" element={<Home />} />
+        <Route path="editor" element={<Editor />} />
+        <Route path="admin" element={<Admin />} />
+        <Route path="lounge" element={<Lounge />} />
+
+        {/* Catch All */}
+        <Route path="*" element={<Missing />} />
+      </Route>
+    </Routes>
   );
 }
 
