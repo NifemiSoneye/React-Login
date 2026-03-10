b26d4d931eb5a97dc25b0043b63498c93181d738
NifemiSoneye <nifemisoneye@gmail.com>
2026-03-10 02:59:50 -0700

    Chapter completed

--- React-Register/src/App.jsx
+++ React-Register/src/App.jsx
@@ -8,7 +8,7 @@ function App() {
 
   return (
     <main className="App">
-      <Register />
+      <Login />
     </main>
   );
 }
--- React-Register/src/Login.jsx
+++ React-Register/src/Login.jsx
@@ -1,6 +1,11 @@
-import { useRef, useState, useEffect, useRef } from "react";
+import { useState, useEffect, useRef, useContext } from "react";
+import AuthContext from "./context/AuthProvider.jsx";
+import axios from "./API/axios";
+
+const LOGIN_URL = "./auth";
 
 const Login = () => {
+  const { setAuth } = useContext(AuthContext);
   const userRef = useRef();
   const errRef = useRef();
   const [user, setUser] = useState("");
@@ -12,55 +17,106 @@ const Login = () => {
     userRef.current.focus();
   }, []);
 
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    try {
+      const response = await axios.post(
+        LOGIN_URL,
+        JSON.stringify({ user, pwd }),
+        {
+          headers: { "Content-Type": "application/json" },
+          withCredentials: true,
+        },
+      );
+      const accessToken = response?.data?.accessToken;
+      const roles = response?.data?.roles;
+      setAuth({ user, pwd, roles, accessToken });
+      setUser("");
+      setPwd("");
+      setSuccess(true);
+      console.log(response);
+    } catch (err) {
+      if (!err.response) {
+        setErrMsg("No server Response");
+      } else if (err.response?.status === 400) {
+        setErrMsg("Missing Username or Password");
+      } else if (err.response?.status === 401) {
+        setErrMsg("Unauthorized");
+      } else {
+        setErrMsg("Login Failed");
+      }
+      errRef.current.focus;
+    }
+    /* console.log(user, pwd); */
+  };
+
   useEffect(() => {
     setErrMsg("");
   }, [user, pwd]);
   return (
-    <section>
-      <p
-        ref={errRef}
-        className={errMsg ? "errmsg" : "offscreen"}
-        aria-live="assertive"
-      >
-        {errMsg}
-      </p>
-      <h1>Sign In</h1>
+    <>
+      {success ? (
+        <section>
+          <h1>Success!</h1>
+          <p>
+            <a href="#">Sign In</a>
+          </p>
+        </section>
+      ) : (
+        <section>
+          <p
+            ref={errRef}
+            className={errMsg ? "errmsg" : "offscreen"}
+            aria-live="assertive"
+          >
+            {errMsg}
+          </p>
+          <h1>Sign In</h1>
 
-      <form>
-        <label
-          htmlFor="
+          <form onSubmit={handleSubmit}>
+            <label
+              htmlFor="
         username"
-        >
-          Username :
-        </label>
+            >
+              Username :
+            </label>
 
-        <input
-          type="text"
-          id="username"
-          ref={userRef}
-          autoComplete="off"
-          onChange={(e) => setUser(e.target.value)}
-          value={user}
-          required
-        />
-        <label
-          htmlFor="
+            <input
+              type="text"
+              id="username"
+              ref={userRef}
+              autoComplete="off"
+              onChange={(e) => setUser(e.target.value)}
+              value={user}
+              required
+            />
+            <label
+              htmlFor="
         password"
-        >
-          Password :
-        </label>
+            >
+              Password :
+            </label>
 
-        <input
-          type="password"
-          id="password"
-          ref={userRef}
-          onChange={(e) => setPwd(e.target.value)}
-          value={pwd}
-          required
-        />
-        <button>Sign In</button>
-      </form>
-    </section>
+            <input
+              type="password"
+              id="password"
+              ref={userRef}
+              onChange={(e) => setPwd(e.target.value)}
+              value={pwd}
+            />
+            <button>Sign In</button>
+          </form>
+          <p>
+            Need an account ? <br />
+            <span className="line">
+              {/* put router link here */}
+              <a href="#">Sign Up</a>
+            </span>
+          </p>
+        </section>
+      )}
+    </>
   );
 };
 
