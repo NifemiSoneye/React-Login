da17aef2186c16e1817ac30e02d95fab4e0e875c
NifemiSoneye <nifemisoneye@gmail.com>
2026-03-16 11:36:36 -0700

    DEV tools disabler installed

--- React-Register/src/main.jsx
+++ React-Register/src/main.jsx
@@ -4,6 +4,7 @@ import "./index.css";
 import App from "./App.jsx";
 import { AuthProvider } from "./context/AuthProvider.jsx";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { disableReactDevTools } from "@fvilers/disable-react-devtools";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
