e1d81e05fdf3077833d2ee0838715f411d032a5f
NifemiSoneye <nifemisoneye@gmail.com>
2026-03-17 08:40:16 -0700

    Functional components added

--- React-login-redux/src/App.jsx
+++ React-login-redux/src/App.jsx
@@ -1,7 +1,24 @@
-import { useState } from "react";
+import { Routes, Route } from "react-router-dom";
+import Layout from "./components/Layout";
+import Public from "./components/Public";
+import Login from "./features/auth/Login";
+import Welcome from "./features/auth/Welcome";
+import RequireAuth from "./features/auth/RequireAuth";
 
 function App() {
-  return <></>;
+  return (
+    <Routes>
+      <Route path="/" element={<Layout />}>
+        {/* Public Routes */}
+        <Route index element={<Public />}></Route>
+        <Route path="login" element={<Public />}></Route>
+        {/* Protected Routes */}
+        <Route element={<RequireAuth />}>
+          <Route path="welcome" element={<Welcome />} />
+        </Route>
+      </Route>
+    </Routes>
+  );
 }
 
 export default App;
